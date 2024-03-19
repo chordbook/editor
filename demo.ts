8fc9bf34dfd507ecece151f4933c0c8d80004e78
Brandon Keepers <brandon@opensoul.org>
2024-03-19 16:41:24 -0400

    Tweak demo content

--- demo.ts
+++ demo.ts
@@ -1,7 +1,24 @@
 import { createEditor } from "./src/"
 
-const el = document.querySelector('#editor')!
-const doc = el.textContent!
-el.textContent = ""
+const doc = `# Welcome to the new ChordBook.app Editor! 🎸
+# This editor is optimized for the ChordPro format (https://www.chordpro.org).
+# This interactive demo will give you an overview of the features.
 
-createEditor(el, { doc })
+{title: Song Title}
+{artist: Artist Name}
+
+{start_of_verse}
+This is a [C]song with chords
+{end_of_verse}
+
+{start_of_chorus}
+This is the [G]chorus
+{end_of_chorus}
+
+# Features:
+# ✅ Syntax Highlighting
+# ✅ Chord autocomplete - type "[" and you will see autocomplete of previously used chords
+# ✅ Snippets - type "title", "start_of…", "tab" or any other ChordPro directive
+`
+
+createEditor(document.querySelector('#editor')!, { doc })
